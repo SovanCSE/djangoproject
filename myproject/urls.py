fcaf41cace38fffdbedee551beac67fc2f55c917
SovanCSE <sovan.panda338@gmail.com>
2019-01-14 02:26:10 +0530

    restapi app is created.

--- myproject/urls.py
+++ myproject/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('firstapp.urls'))
+    path('',include('firstapp.urls')),
+    path('api/',include('restapi.urls'))
+    
 ]
